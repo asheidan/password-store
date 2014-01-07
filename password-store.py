ad8e9bc3dea7f9419de3be304fc65c169155b91b
Emil Eriksson <asheidan@gmail.com>
2014-01-07 18:21:12 +0100

    Colorized output of tree

--- password-store.py
+++ password-store.py
@@ -8,6 +8,24 @@ import re
 import subprocess
 import sys
 
+class Colorizer(object):
+
+    red = 91
+    green = 92
+    yellow = 93
+    blue = 94
+
+    def color(self, code=0):
+        return "\033[%dm" % code
+
+    def colorize(self, string, color_code):
+        return self.color(color_code) + string + self.color()
+
+    def directory(self, string):
+        return self.colorize(string, self.blue)
+
+color = Colorizer()
+
 ### Backends ##################################################################
 
 class BaseBackend(object):
@@ -48,13 +66,6 @@ class ClearTextBackend(BaseBackend):
         content = sys.stdin.read()
         with open(os.path.join(self.root, key)) as file:
             file.write(content)
-
-    def display(self, matcher=None, subdir=None):
-        keys = []
-        for key in self.list():
-            if matcher and matcher.matches(key):
-                keys.append(key)
-        display_keys(keys)
 
 
 class GPGBackend(ClearTextBackend):
@@ -145,8 +156,6 @@ def backends():
         if CONFIG_FILE_NAME in files:
             del subdirs[:]
 
-            print(path, "is a backend")
-
             config_file = os.path.join(path, CONFIG_FILE_NAME)
             config = configparser.SafeConfigParser()
             config.read(config_file)
@@ -211,8 +220,6 @@ def parse_commandline():
     help_parser = subparsers.add_parser('help', help='show help')
     help_parser.add_argument('help_command', metavar='command', nargs='?')
 
-    #print(subparsers.choices)
-
     args = parser.parse_args()
     if 'help' == args.command:
         if args.help_command is None:
@@ -227,10 +234,59 @@ def create():
     pass
 
 
+def pad_sub(line, last=False):
+    if not last:
+        return "\u2502   " + line
+    else:
+        return "    " + line
+
+def pad_this(line, last=False):
+    if not last:
+        return "\u251C\u2500\u2500 " + line
+    else:
+        return "\u2514\u2500\u2500 " + line
+
+
+def display_directory(directory, matcher=None):
+    rows = []
+    walker = os.walk(directory, followlinks=True)
+    path, subs, files = next(walker)
+    if CONFIG_FILE_NAME in files:
+        files.remove(CONFIG_FILE_NAME)
+        #rows.append("\u2502 Backend")
+
+    for i, subdir in enumerate(sorted(subs)):
+        is_last = len(files) == 0 and i >= len(subs) - 1
+        walk_root = os.path.join(directory, subdir)
+        rows.append(pad_this(color.directory(subdir), is_last))
+        subdir_data = display_directory(walk_root, matcher)
+        for data_line in subdir_data:
+            rows.append(pad_sub(data_line, is_last))
+
+    for i, file in enumerate(sorted(files)):
+        is_last = i >= len(files) - 1
+        rows.append(pad_this(file, is_last))
+
+    return rows
+
+
+def display(directory, matcher=None):
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+    code = locale.getpreferredencoding()
+
+    #print(code)
+
+    print(directory)
+    print("\n".join(display_directory(directory, matcher)))
+
+
 ###############################################################################
 
 if '__main__' == __name__:
     args = parse_commandline()
 
+    
     if 'list' == args.command:
-        backends()
+        display('storage')
+
